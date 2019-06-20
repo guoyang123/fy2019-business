d776dbff364fa2d412bf7f13bcf50c03d8b5d1e9
gy <1035610621@qq.com>
2019-06-20 15:56:26 +0800

    docker部署

---
+++ business/readme.txt
@@ -0,0 +1,4 @@
+启动项目:
+docker run -it -p 8888:8888 -v /neuedu/qrcode:/neuedu/qrcode  monotnous/fy19-business:5.0-snapshot /bin/bash
+
+
--- business/src/main/java/com/neuedu/schedule/CloseOrderSchedule.java
+++ business/src/main/java/com/neuedu/schedule/CloseOrderSchedule.java
@@ -30,15 +30,12 @@ public class CloseOrderSchedule {
 
     @Scheduled(cron = "0/10 * * * * ?")
     public  void   closeOrder(){
-
        //step1:获取redis的锁
-
       Long result=shardedRedisApi.setnx(Const.LOCK_KEY,String.valueOf(System.currentTimeMillis()+lockTimeout));
       if(result==1){ //获取redis锁
          //设置锁的过期时间
           close();
       }else{
-
           //是否有权利获取锁
           String value=shardedRedisApi.get(Const.LOCK_KEY);
           if(value==null||(value!=null&& System.currentTimeMillis()>Long.parseLong(value))){
@@ -49,21 +46,9 @@ public class CloseOrderSchedule {
              }else{
                  System.out.println("没有获取到锁");
              }
-
-
-
-
           }
-
-
-
-
       }
-
-
     }
-
-
 
     public  void close(){
         shardedRedisApi.expire(Const.LOCK_KEY,20);
